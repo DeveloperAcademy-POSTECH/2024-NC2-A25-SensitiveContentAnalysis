b6f2073cfc8b1942d75eaa78a891747d6fe946f7
JuHyeon Byun <wngus4296@swu.ac.kr>
2024-06-18 20:46:08 +0900

    chore: 폰트 추가

---
+++ Ungteoring/Ungteoring/Sources/Global/Extensions/UIFont+.swift
@@ -0,0 +1,20 @@
+//
+//  UIFont+.swift
+//  Ungteoring
+//
+//  Created by Juhyeon Byun on 6/18/24.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    static func pretendardB(_ size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: size)!
+    }
+    
+    static func pretendardR(_ size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-Regular", size: size)!
+    }
+    
+}
