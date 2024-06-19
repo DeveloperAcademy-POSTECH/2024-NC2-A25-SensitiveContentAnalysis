1d193df744197efeaad1ba0d4c7bdeb6985aa4b8
JuHyeon Byun <wngus4296@swu.ac.kr>
2024-06-19 21:11:44 +0900

    feat: 몇시간 전, 몇  분 전 로직 수정

--- Ungteoring/Ungteoring/Sources/Global/Extensions/Date+.swift
+++ Ungteoring/Ungteoring/Sources/Global/Extensions/Date+.swift
@@ -8,24 +8,23 @@
 import Foundation
 
 enum DateFormatType: String {
-    case dateDayTime = "yyyy.MM.dd (E) HH:mm"
-    case isoDateTime = "yyyy-MM-dd'T'HH:mm:ss"
+    case dateDayTime = "yyyy.MM.dd HH:mm"
 }
 
 extension DateFormatType {
-
+    
     var formatter: DateFormatter {
         guard let formatter = DateFormatType.cachedFormatters[self] else {
             let generatedFormatter = DateFormatType.makeFormatter(withDateFormat: self)
             DateFormatType.cachedFormatters[self] = generatedFormatter
             return generatedFormatter
         }
-
+        
         return formatter
     }
-
+    
     private static var cachedFormatters: [DateFormatType: DateFormatter] = [:]
-
+    
     private static func makeFormatter(withDateFormat dateFormatType: DateFormatType) -> DateFormatter {
         let formatter = DateFormatter()
         formatter.dateFormat = dateFormatType.rawValue
@@ -36,18 +35,21 @@ extension DateFormatType {
 }
 
 extension Date {
-
+    
     func format(_ formatType: DateFormatType) -> String {
-        return formatType.formatter.string(from: self)
-    }
-    
-    func getBetweenDay(to endDate: Date) -> Int {
-        let calendar = Calendar.current
-        let startMidnight = calendar.startOfDay(for: self)
-        let endMidnight = calendar.startOfDay(for: endDate)
+        let offsetComps = Calendar.current.dateComponents([.year,.month,.day,.hour,.minute], from: self, to: Date())
         
-        let components = calendar.dateComponents([.day], from: startMidnight, to: endMidnight)
-        return components.day ?? 0
+        if case let (h?, m?) = (offsetComps.hour, offsetComps.minute) {
+            if h == 0 {
+                return "\(m)분 전"
+            } else if h < 24 {
+                return "\(h)시간 전"
+            } else {
+                return formatType.formatter.string(from: self)
+            }
+        }
+        
+        return "-"
     }
-    
+
 }
