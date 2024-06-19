fe648ce8c208c2baa2219b2d7e0f2f22a1829f55
JuHyeon Byun <wngus4296@swu.ac.kr>
2024-06-19 20:40:41 +0900

    chore: 로띠 -> gif

--- Ungteoring/Ungteoring/Sources/UILayer/Splash/SplashViewController.swift
+++ Ungteoring/Ungteoring/Sources/UILayer/Splash/SplashViewController.swift
@@ -12,25 +12,66 @@ final class SplashViewController: UIViewController {
     
     // MARK: UI Component
     
-    private let animationView: LottieAnimationView = .init(name: "mp4convert")
+    private let gifImageView = {
+            let view = UIImageView()
+            view.translatesAutoresizingMaskIntoConstraints = false
+            return view
+        }()
     
     // MARK: Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .black
-        view.addSubview(animationView)
-        
-        animationView.frame = view.bounds
-        animationView.center = view.center
-        animationView.contentMode = .scaleAspectFit
-        animationView.loopMode = .repeat(1)
-        animationView.play()
-        
+        setUI()
+        setGIF()
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             self.dismiss(animated: false)
         }
     }
     
+}
+
+// MARK: - Methods
+
+extension SplashViewController {
+    
+    private func setGIF() {
+        guard
+            let gifURL = Bundle.main.url(forResource: "splash", withExtension: "gif"),
+            let gifData = try? Data(contentsOf: gifURL),
+            let source = CGImageSourceCreateWithData(gifData as CFData, nil)
+        else { return }
+        
+        let frameCount = CGImageSourceGetCount(source)
+        var images = [UIImage]()
+
+        (0..<frameCount)
+            .compactMap { CGImageSourceCreateImageAtIndex(source, $0, nil) }
+            .forEach { images.append(UIImage(cgImage: $0)) }
+
+        gifImageView.animationImages = images
+        gifImageView.animationDuration = TimeInterval(frameCount) * 0.05
+        gifImageView.animationRepeatCount = 0
+        gifImageView.startAnimating()
+    }
+    
+}
+
+// MARK: - UI
+
+extension SplashViewController {
+    
+    private func setUI() {
+        view.addSubview(gifImageView)
+        view.backgroundColor = .black
+        
+        setConstraints()
+    }
+    
+    private func setConstraints() {
+        gifImageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
 }
